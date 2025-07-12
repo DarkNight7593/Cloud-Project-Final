78e953e18396b6457d67defd03b0c0dadb053848
DarkNight7593 <juan.inca@utec.edu.pe>
2025-07-12 18:13:00 -0500

    error

--- Api-Org/Lambda_BuscarOrg.py
+++ Api-Org/Lambda_BuscarOrg.py
@@ -16,8 +16,10 @@ def json_serial(obj):
 def lambda_handler(event, context):
     try:
         # Obtener tenant_id desde query parameters (GET)
+        print("EVENT: ", json.dumps(event),default=json_serial)  # O usa logger si prefieres
+
         tenant_id = event.get('params', {}).get('querystring', {}).get('tenant_id')
-        
+
         if not tenant_id:
             return {
                 'statusCode': 400,
